5166302408feddfd1b945e67fed943fbf404ed46
JanMajer86 <majer.jan.1986@gmail.com>
2024-06-06 15:40:09 +0200

    012 Implementing Error and Success Messages

--- src/js/model.js
+++ src/js/model.js
@@ -1,6 +1,7 @@
 import { async } from 'regenerator-runtime';
 import { API_URL } from './config.js';
 import { getJSON } from './helpers.js';
+import recipeView from './views/recipeView.js';
 
 export const state = {
   recipe: {},
@@ -24,5 +25,6 @@ export const loadRecipe = async function (id) {
   } catch (err) {
     // Temp error handling
     console.error(`${err} 🤬🤬🤬🤬🤬`);
+    throw err;
   }
 };
