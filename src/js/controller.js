cd8b5c0620f093e84b00093b14abd020a53ec184
JanMajer86 <majer.jan.1986@gmail.com>
2024-06-06 14:25:14 +0200

    publisher-subscriber-pattern

--- src/js/controller.js
+++ src/js/controller.js
@@ -20,8 +20,7 @@ const controlRecipes = async function () {
   }
 };
 
-// controlRecipes();
-
-['hashchange', 'load'].forEach(ev =>
-  window.addEventListener(ev, controlRecipes)
-);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
