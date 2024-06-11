443a3f2b141dd5a4b492182e7b5103c4d2f182af
JanMajer86 <majer.jan.1986@gmail.com>
2024-06-11 09:15:45 +0200

    pagination handler

--- src/js/controller.js
+++ src/js/controller.js
@@ -49,8 +49,13 @@ const controlSearchResults = async function () {
   }
 };
 
+const controlPagination = function () {
+  console.log('Page controller');
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 init();
